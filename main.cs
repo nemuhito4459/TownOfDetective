e92c97ecc91508fbfc98438dca5c5c9d52d9f56f
nemuhito4459 <nemuhito.59@gmail.com>
2026-03-27 19:14:21 +0900

    main.cs

--- main.cs
+++ main.cs
@@ -29,17 +29,15 @@ namespace TownOfHost
     {
         // == プログラム設定 / Program Config ==
         // modの名前 / Mod Name (Default: Town Of Host)
-        public static readonly string ModName = "Town Of Host-K";
+        public static readonly string ModName = "Town Of Detective";
         // modの色 / Mod Color (Default: #00bfff)
-        public static readonly string ModColor = "#00c1ff";
+        public static readonly string ModColor = "#44617b";
         // 公開ルームを許可する / Allow Public Room (Default: true)
-        public static readonly bool AllowPublicRoom = true;
+        public static readonly bool AllowPublicRoom = false;
         // フォークID / ForkId (Default: OriginalTOH)
-        public static readonly string ForkId = "TOH-K";
+        public static readonly string ForkId = "TOD";
         // Discordボタンを表示するか / Show Discord Button (Default: true)
-        public static readonly bool ShowDiscordButton = true;
-        // Discordサーバーの招待リンク / Discord Server Invite URL (Default: https://discord.gg/W5ug6hXB9V)
-        public static readonly string DiscordInviteUrl = "https://discord.gg/5DPqH8seFq";
+        public static readonly bool ShowDiscordButton = false;
         // ==========
         public const string OriginalForkId = "OriginalTOH"; // Don't Change The Value. / この値を変更しないでください。
         // == 認証設定 / Authentication Config ==
